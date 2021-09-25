2343696ee8b3a3343200070d85443d35450d0bf8
bonillasan <sbonilla0197@gmail.com>
2021-09-25 14:11:57 -0500

    waiting sql

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'list', loadChildren: () => import('./pages/users/list/list.module').then(m => m.ListModule) }, { path: 'new', loadChildren: () => import('./pages/users/new/new.module').then(m => m.NewModule) }, { path: 'details', loadChildren: () => import('./pages/users/details/details.module').then(m => m.DetailsModule) }, { path: 'edit', loadChildren: () => import('./pages/users/edit/edit.module').then(m => m.EditModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
